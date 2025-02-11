b06816d9a838df6d4b37f06757bc3e17c04f6e0b
vaichat <vaifa@qq.com>
2025-02-11 19:37:27 +0800

    Site updated: 2025-02-11 19:37:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/11/hello-world/","2023/10/10/文章标题/"];function toRandomPost(){
+var posts=["2025/02/11/文章标题-1/","2025/02/11/hello-world/","2023/10/10/文章标题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
